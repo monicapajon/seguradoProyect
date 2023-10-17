790aef9d17a91259eefa9602c1576f8897126ec7
Monica Pajon <moniestudio161@gmail.com>
2023-10-17 18:41:28 -0300

    Se agrego check y search

--- dom.js
+++ dom.js
@@ -1,5 +1,51 @@
+//--------------------------------- CHECKBOX------------------------------------------
+// Obtener el contenedor de búsqueda
+const searchContainer = document.getElementById('search');
+
+// Crear un div para los checkboxes
+const checkboxDiv = document.createElement('div');
+checkboxDiv.classList.add('field', 'is-pulled-left'); // Usamos 'is-pulled-left' para alinear a la izquierda
+
+// Crear etiquetas para cada checkbox
+const categories = ["Food", "Museum", "Concert", "Race", "Books", "Cinema", "Party"];
+
+categories.forEach(category => {
+  const checkboxLabel = document.createElement('label');
+  checkboxLabel.classList.add('checkbox');
+
+  const checkboxInput = document.createElement('input');
+  checkboxInput.setAttribute('type', 'checkbox');
+  checkboxInput.setAttribute('id', category.toLowerCase()); // Usamos el nombre de la categoría en minúsculas como ID
+
+  checkboxLabel.appendChild(checkboxInput);
+  checkboxLabel.appendChild(document.createTextNode(category));
+
+  checkboxDiv.appendChild(checkboxLabel);
+});
+
+// Agregar el div de checkboxes al contenedor de búsqueda
+searchContainer.appendChild(checkboxDiv);
+
+
+//----------------------------------- SEARCH--------------------------------------------
+
+// Crear un div para el campo de búsqueda
+const searchDiv = document.createElement('div');
+searchDiv.classList.add('navbar-item', 'is-pulled-right'); // Usamos 'is-pulled-right' para alinear a la derecha
+
+// Crear el campo de búsqueda
+const searchInput = document.createElement('input');
+searchInput.classList.add('input');
+searchInput.setAttribute('type', 'text');
+searchInput.setAttribute('placeholder', 'Buscar eventos');
+
+// Agregar el campo de búsqueda al div
+searchDiv.appendChild(searchInput);
+searchContainer.appendChild(searchDiv);
+
+//----------------------------------- CARDS----------------------------------------------
 // @ts-nocheck
-function showEvents(eventsToShow){
+function showEvents(eventsToShow) {
   const cardsContainer = document.getElementById('cards');
   const cardTemplate = document.getElementById('card-template');
   // @ts-ignore
@@ -9,7 +55,7 @@ function showEvents(eventsToShow){
     const newCard = cardTemplate?.content.cloneNode(true);
     const img = newCard.querySelector('img');
     img.src = x.image
-    
+
 
     cardsContainer?.appendChild(newCard)
   })
